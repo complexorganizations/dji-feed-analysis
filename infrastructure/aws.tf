3ce05f2a9a7350382d7e92ad30a5cb40d602ccc6
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <prajwal.koirala.0000@protonmail.com>
2023-03-08 11:47:05 -0500

    Update aws.tf

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -17,3 +17,70 @@ module "vpc" {
   reuse_nat_ips       = true
   enable_vpn_gateway = true
 }
+
+// Security Groups
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+}
+
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
+// S3
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  versioning = {
+    enabled = true
+  }
+}
+
+// EC2
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  for_each = toset(["one", "two", "three"])
+  name = "instance-${each.key}"
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
